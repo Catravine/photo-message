5de2abb392d94d291a9cffbbd497a53fd441e88d
Caroline Courtney <caroline@carolinecourtney.com>
2019-03-31 17:43:59 -0400

    displaying photos & messages

--- app/main.js
+++ app/main.js
@@ -31,12 +31,44 @@ const _init = () => {
             toastr.warning("Photo & Caption required", 'Incomplete Message');
             return
         }
-        console.log('adding message');
-        console.log(caption)
+
+        // Render new message in feed
+        renderMessage({ photo: camera.photo, caption });
 
         // Reset caption field on success
         $('#caption').val('');
         $('#camera').css('background-image', '').removeClass('withPhoto')
         camera.photo = null;
     });
+}
+
+// Create new message element
+const renderMessage = (message) => {
+
+    // Message html
+    let msgHTML = `
+        <div style="display:none;" class="row message bg-light mb-2 rounded shadow">
+            <div class="col-2 p-1">
+                <img src="${message.photo}" class="photo w-100" rounded">
+            </div>
+            <div class="col-10 p-1">${message.caption}</div>
+        </div>
+    `;
+
+    // Prepend to Messages
+    $(msgHTML).prependTo('#messages').show(500)
+
+        // bind a click handler on new image element to show in modal
+        .find('img').on("click", showPhoto)
+};
+
+// Show message photo in Modal
+const showPhoto = (e) => {
+
+    // get photo src
+    let photoSrc = $(e.currentTarget).attr('src');
+
+    // set to and show photo frame Modal
+    $('#photoframe img').attr('src', photoSrc);
+    $('#photoframe').modal('show');
 }
